80566d4d2bc57e31a9d25410d1e636028c99850b
Susan Vanderplas <S1Vande@nppd.com>
2016-01-19 08:28:31 -0600

    Fixes to applet, css - ready for production?

--- server.R
+++ server.R
@@ -10,75 +10,111 @@ library(tm)
 library(stringr)
 library(magrittr)
 library(dplyr)
-library(wordcloud)
+library(RColorBrewer)
+library(nppd)
 
-stemCompletion_mod <- function(x, dict, type = "shortest") {
-  x %>% as.character() %>%
-    strsplit(split = " ") %>%
-    paste() %>%
-    stemCompletion(dictionary = dict, type = type) %>%
-    paste(sep = "", collapse = " ") %>%
-    stripWhitespace() %>%
-    PlainTextDocument
+# --- CNS specific functions ---------------------------------------------------
+
+cns_proc_regex <- "\\b(?!CNS-\\d{4}-\\d{5})(?!1-\\d{7})(?!1-[DHNPR])(?!9-HV)(?!9-SS)(?!201[123456])(?!0\\.\\d{1,3}[E]\\d{1,3})(?!\\d\\.\\d{1,3}-\\d\\.\\d{1,3})(?!\\d{3}-\\d{3,4})(?!\\d{1,2}-\\d{1,2}-\\d{2,4})(?!\\d{2}-\\d{2})(?!SS-)(?!EE-)(\\d{1}|1\\d{1}|ENN?)[\\._-][A-Z\\d\\._-]{1,}\\b"
+
+cfr_regex <- "\\b(\\d{1,2}CFR[[:alnum:]\\.]*(?:[[:punct:]]{1}[[:alnum:]]{1,}[[:punct:]]{1})?)"
+
+cns_cr_regex <- "\\b((?:CNS|CR|CR[ -]CNS)[ -]\\d{4}[ -]\\d{4,5})\\b"
+
+cns_wo_regex <- "\\b((?:WO|wo|W\\.O\\.|work order|Work Order)[ #-]*\\d{6,8})\\b"
+
+cns.stopwords <- c("per", "will", "method", "of", "discovery", "condition", "description", "requirement", "require", "not", "met", "action", "attached", "document", "cns", "attachment", "also", "however", "says")
+
+# Fixes raw input text before processing
+cns_fix_input_text <- function(x) {
+  x %>%
+    str_replace_all("\\n|\\r|\\\"|/", " ") %>%
+    str_replace_all(cns_proc_regex, " ") %>%
+    str_replace_all(cfr_regex, " ") %>%
+    str_replace_all(cns_cr_regex, " ") %>%
+    str_replace_all(cns_wo_regex, " ") %>%
+    str_to_lower() %>%
+    str_replace_all("ann -", " ann ") %>%
+    str_replace_all("[\\b ]sh[\\b ]", " sheet ") %>%
+    str_replace_all("[\\b ]work order[\\b#\\. ]", " workorder ") %>%
+    str_replace_all("[\\b ]wo[\\b #-]", " workorder ") %>%
+    str_replace_all("[\\b ]dg[\\b ]", " diesel generator ") %>%
+    str_replace_all("[\\b ]non[- ](\\w)", " non\\1") %>%
+    str_replace_all("[\\b ]sap[\\b ]", "sssaaappp") %>%
+    str_replace_all("[\\b ]performance deficiency[\\b ]", " performancedeficiency ") %>%
+    str_replace_all("[\\b ]operator[\\b ]", "operatorperson") %>%
+    str_replace_all("[\\b ]ops[\\b ]", " operationsdept ") %>%
+    str_replace_all("ca&a", "correctiveactionassess") %>%
+    str_replace_all("ep&c", "eeepppccc") %>%
+    str_replace_all("[\\b ]pi | performance indicators?[\\b ]", " performanceindicator ") %>%
+    str_replace_all("[\\b ]clearance order[\\b ]", " clearanceorder ") %>%
+    str_replace_all("[\\b ]gnf[\\b ]", " globalnuclearfuels ") %>%
+    str_replace_all("[\\b ]g[\\. ]?e[\\. ]?(h?)[\\b ]", " generalelectric\\1 ") %>%
+    str_replace_all("[ \\b]cat[ \\\"\\(-]{1,}([abcdwq])(?:[\\\"\\)])?(?:[[:punct:]]([HL])[[:punct:]])?[\\b \\.]", " classificationcategory\\1\\2 ") %>%
+    str_replace_all("[ \\b]classificationcategory([abcdwq])[ -]([trendcboa]{5})[ \\b]",
+                    " classificationcategory\\1\\2 ")
 }
 
-MakeWordFreq <- function(wordlist, stem = T, rm.stopwords = T,
-                         stopword.list = NULL){
-  # Function to make a word frequency table
-  wordlist %<>%
-    paste(., collapse = " ") %>%
-    VectorSource() %>%
-    Corpus() %>%
-    tm_map(., removeNumbers) %>%
-    tm_map(., removePunctuation)
-
-  if (rm.stopwords) {
-    wordlist %<>%
-    tm_map(., removeWords, stopwords("en"))
-  }
-
-  wordlist %<>%
-    TermDocumentMatrix %>%
-    as.matrix() %>%
-    as.data.frame() %>%
-    set_names("freq")
-
-  if (stem) {
-    wordlist %<>%
-      mutate(original = row.names(.), stems = stemDocument(row.names(.)))
-  } else {
-    wordlist %<>%
-      mutate(original = row.names(.), stems = row.names(.))
-  }
-
-  wordlist %<>%
-    group_by(stems) %>%
-    summarize(freq = sum(freq), original = original[which.min(nchar(original))]) %>%
-    ungroup() %>%
-    select(-stems) %>%
-    set_names(c("freq", "word")) %>%
-    select(word, freq)
-
-  if (rm.stopwords) {
-    wordlist %<>%
-      subset(!word %in% c(stopwords("en"), stopword.list))
-  }
-
-  wordlist %<>%
-    arrange(desc(freq))
+# Fixes text after processing so that it is displayed correctly
+cns_fix_word_freq <- function(x) {
+  str_replace_all(
+    x,
+    c("performanceindicator" = "performance indicator",
+      "performancedeficiency" = "performance deficiency",
+      "clearanceorder" = "clearance order",
+      "eeepppccc" = "EP&C",
+      "sssaaappp" = "SAP",
+      "correctiveactionassess" = "CA&A",
+      "generalelectrich" = "GEH",
+      "generalelectric" = "GE",
+      "globalnuclearfuels" = "GNF",
+      "classificationcategory" = "CAT ",
+      "operatorperson" = "operator",
+      "operationsdept" = "Operations",
+      "^cfr$" = "CFR",
+      "^ero$" = "ERO",
+      "^annann$" = "ANN-ANN",
+      "^eof$" = "EOF",
+      "^rhr$" = "RHR",
+      "^jic$" = "JIC",
+      "^qc$" = "QC",
+      "^epip$" = "EPIP",
+      "^mro$" = "MRO",
+      "^nis$" = "NIS",
+      "^tsc$" = "TSC",
+      "^tpp$" = "TPP",
+      "^ann$" = "ANN",
+      "^cda$" = "CDA",
+      "^en$" = "EN",
+      "^dg$" = "DG",
+      "^ep$" = "EP",
+      "^re$" = "RE",
+      "^ro$" = "RO",
+      "^rp$" = "RP",
+      "^cr$" = "CR",
+      "^qa$" = "QA",
+      "^cas$" = "CAS",
+      "^cns$" = "CNS",
+      "^erp$" = "ERP",
+      "alara" = "ALARA",
+      "^pmis$" = "PMIS",
+      "^cooper$" = "Cooper",
+      "^nrc$" = "NRC", "^wano$" = "WANO", "^inpo$" = "INPO")
+  )
 }
 
-MakeWordcloud <- function(x, color.set = brewer.pal(6, "Dark2"), max.words = 50,  ...){
-  wordcloud(x$word, x$freq, colors = color.set, random.color = T, max.words = max.words, random.order = F, rot.per = .3, ...)
+collapse_values <- function(x){
+  lapply(x, function(i) paste(i, collapse = ", "))
 }
 
+# ------------------------------------------------------------------------------
 
 shinyServer(function(input, output) {
 
   output$wordcloud <- renderPlot({
     validate(
       need(input$words != "",
-           "Please paste text into the box labeled \"Wordcloud text\"")
+           "Please paste text into the box labeled \"Wordcloud text\".")
     )
 
     if (input$stopwords) {
@@ -87,9 +123,28 @@ shinyServer(function(input, output) {
       stopword.list <- NULL
     }
 
+    if (input$CNSstopwords) {
+      stopword.list <- c(stopword.list, cns.stopwords)
+    }
+
     color.pal <- brewer.pal(6, input$palette)
     if (input$palette == "Greys") {
       color.pal <- brewer.pal(8, input$palette)[-c(1:2)]
+    }
+
+    if (input$fixCNS) {
+      input$words %>% cns_fix_input_text() %>%
+        MakeWordFreq(stopword.list = stopword.list,
+                     stem = input$stem, rm.stopwords = input$stopwords) %>%
+        mutate(word = cns_fix_word_freq(word)) %>%
+        MakeWordcloud(color.set = color.pal, max.words = input$nWords,
+                      min.freq = ifelse(nrow(.) > 50, 3, 2))
+    } else {
+      input$words %>%
+        MakeWordFreq(stopword.list = stopword.list,
+                     stem = input$stem, rm.stopwords = input$stopwords) %>%
+        MakeWordcloud(color.set = color.pal, max.words = input$nWords,
+                      min.freq = ifelse(nrow(.) > 50, 3, 2))
     }
 
     data <- str_split(input$words, "\\n") %>%
--- ui.R
+++ ui.R
@@ -8,14 +8,14 @@
 library(shiny)
 
 wordcloud_panel <- tabPanel(
-  "WordCloud",
+  "Word Clouds",
   fluidRow(
     column(
       10, offset = 1,
       wellPanel(
         fluidRow(
           column(
-            4,
+            5,
             tags$label("Wordcloud text"),
             br(),
             tags$textarea(id = "words", style = "min-width:100%; min-height = 100px;",
@@ -23,28 +23,40 @@ wordcloud_panel <- tabPanel(
                           rows = 8, cols = 40, autofocus = T)
           ),
           column(
-            2,
+            3,
             tags$label("List of words to ignore"),
-            helpText("Words with fewer than 3 characters will be ignored by default."),
             tags$textarea(id = "ignore", rows = 4, cols = 20,
-                          placeholder = "Ignored words", maxlength = 5000)
-          ),
-          column(
-            2,
-            checkboxInput(inputId = "stopwords",
-                          "Remove common English words", value = T),
-            helpText("Words like: the, our, who, whom, have, has"),
-            checkboxInput(inputId = "stem", "Remove word stems?", value = T),
-            helpText("Word stems, such as -s, -ed, -ing, are removed.",
-                     "Words are replaced with the most common variant")
+                          style = "min-width:100%;",
+                          placeholder = "Ignored words", maxlength = 5000),
+            helpText("Words with 1 or 2 characters are ignored by default.")
           ),
           column(
             4,
             sliderInput("nWords", "Maximum words to include",
-                        min = 15, max = 200, value = 50),
+                        min = 25, max = 200, value = 50, step = 25),
             selectInput("palette", "Color Options",
                         choices = c("Dark" = "Dark2", "Paired" = "Paired",
                                     "Rainbow" = "Set1", "Grey" = "Greys"))
+          )
+        ),
+        fluidRow(
+          column(
+            4,
+            checkboxInput(inputId = "stopwords",
+                          "Remove common English words", value = T),
+            helpText("Words like: the, our, who, whom, have, has"),
+            br(),
+            checkboxInput(inputId = "stem", "Remove word stems?", value = T),
+            helpText("Word stems, such as -s, -ed, -ing, are removed.",
+                     "Words are replaced with the most common variant which appears in the text.")
+          ),
+          column(
+            8,
+            checkboxInput(inputId = "fixCNS", "Fix CNS-related words?", value = F),
+            helpText("Capitalizes common acronyms, prevents \"operator\" and \"operations\" from being grouped with words like \"operate(d)\", and removes work order numbers, procedure numbers, and CR references from the base text."),
+            br(),
+            checkboxInput(inputId = "CNSstopwords", "Remove common CNS CR words", value = F),
+            helpText("Words such as \"condition\", \"description\", \"attached/attachment\", \"CNS\"")
           )
         )
       )
@@ -64,7 +76,10 @@ wordcloud_panel <- tabPanel(
 
 shinyUI(
   navbarPage(
-    "Analysis Tools",
+    title = "Analysis Tools",
+    theme = "bootstrap.css",
+    header = includeCSS("www/extra.css"),
+    inverse = TRUE,
     wordcloud_panel
   )
 )
